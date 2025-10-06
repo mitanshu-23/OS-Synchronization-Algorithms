d087d876bf0f9f1d36a5b53acc76d8fff85e717c
mitanshu-23 <kelaiyamitanshu@gmail.com>
2025-10-06 13:28:15 +0530

    Initial Race Condition Commit'

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use os_synchronization_algorithms::race_condition;
+
 fn main() {
     println!("Hello, world!");
+    race_condition::test();
 }
---
+++ src/race_condition/mod.rs
@@ -0,0 +1,5 @@
+// Here it will be demonstrated the effect of race condition
+
+pub fn test(){
+    println!("Race Condition Callable");
+}

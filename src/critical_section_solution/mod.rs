5e4bcdc5e027dd9980486afbf835a59ffbdf0bf4
mitanshu-23 <kelaiyamitanshu@gmail.com>
2025-10-06 15:24:31 +0530

    Initial Setup of critical_section_solution

---
+++ src/critical_section_solution/mod.rs
@@ -0,0 +1,8 @@
+// 3 needs to be strictly followed
+//Mutual Exclusion
+//Progress
+//Bounded Waiting
+
+pub fn using_lock(){
+    println!("Using Lock");
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 pub mod race_condition;
+pub mod critical_section_solution;

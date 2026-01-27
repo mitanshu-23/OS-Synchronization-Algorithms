f1bf26c02e997d5d5ef31644c88f95c090590308
mitanshu-23 <kelaiyamitanshu@gmail.com>
2026-01-27 15:36:50 +0530

    temp

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-// By default, Rust prevents race conditions in safe code.  
-// However, you can simulate a race condition by using an `unsafe` block.  
-//
+// By default, Rust prevents race conditions in safe code.
+// However, you can simulate a race condition by using an `unsafe` block.
+
 // Our goal is to demonstrate how to solve or mitigate this race condition **while still using unsafe Rust**.
 // That is, the solution will be applied on top of the unsafe code, without removing the `unsafe` block
 // or relying on Rust's safety guarantees.
